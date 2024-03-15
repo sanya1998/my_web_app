328d072e665b0171198f9e8c7922ad37874e56b5
Alexander Desyatov <sanya_1998@mail.ru>
2024-03-15 14:19:35 +0300

    validation of body

--- app/api/v1/hotels.py
+++ app/api/v1/hotels.py
@@ -3,7 +3,15 @@ from typing import List, Optional
 from fastapi import APIRouter, Query
 from datetime import date
 
+from pydantic import BaseModel
+
 router = APIRouter(prefix="/hotels", tags=["hotels"])
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
 
 
 @router.get("/{hotel_id}")
@@ -20,3 +28,8 @@ async def get_hotels(
         stars: Optional[int] = Query(default=None, ge=1, le=5),
 ) -> List[dict]:
     return [{"hotel_id": 1, "hotel_name": "Hotel Name", "date_from": date_from, "date_to": date_to}]
+
+
+@router.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
