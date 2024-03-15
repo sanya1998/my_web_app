548362c128ff1825914cc10bd619d14cb425f8a2
Alexander Desyatov <sanya_1998@mail.ru>
2024-03-15 14:31:48 +0300

    4-7 validation of output

--- app/api/v1/hotels.py
+++ app/api/v1/hotels.py
@@ -14,6 +14,11 @@ class SBooking(BaseModel):
     date_to: date
 
 
+class SHotel(BaseModel):
+    hotel_id: int
+    name: str
+
+
 @router.get("/{hotel_id}")
 async def get_hotel(hotel_id: int) -> dict:
     return {"hotel_id": hotel_id, "hotel_name": "Hotel Name"}
@@ -26,8 +31,10 @@ async def get_hotels(
         date_to: date,
         has_spa: Optional[bool] = None,
         stars: Optional[int] = Query(default=None, ge=1, le=5),
-) -> List[dict]:
-    return [{"hotel_id": 1, "hotel_name": "Hotel Name", "date_from": date_from, "date_to": date_to}]
+) -> List[SHotel]:
+    hotels = [{"hotel_id": 1, "name": "Hotel Name"}]
+    return hotels
+    # return [{"hotel_id": 1, "hotel_name": "Hotel Name", "date_from": date_from, "date_to": date_to}]
 
 
 @router.post("/bookings")
