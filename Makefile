676533653b366180547989418d2f424d31d6b90e
Alexander Desyatov <sanya_1998@mail.ru>
2024-10-10 21:21:29 +0300

    2-2-2 add redis

--- Makefile
+++ Makefile
@@ -36,4 +36,4 @@ forward_migrations_head_local: set_local_env alembic_upgrade_head
 rollback_one_migration_local: set_local_env alembic_downgrade_1
 
 up-services:
-	docker-compose --env-file .envs/local.env up -d postgres
+	docker-compose --env-file .envs/local.env up -d postgres redis
--- app/config/db.py
+++ app/config/db.py
@@ -7,33 +7,35 @@ class DbSettings(BaseSettings):
     Конфигурация базы данных
     """
 
-    POSTGRES_VERSION: float
+    # REDIS
+    REDIS_VERSION: str
+    REDIS_DEFAULT_PASSWORD: str
+    REDIS_USER: str
+    REDIS_USER_PASSWORD: str
+    REDIS_PORT: int
 
-    DB_DRIVER: str = "postgresql+asyncpg"
-
-    DB_HOST: str
-    DB_PORT: int
-
-    DB_USER: str
-    DB_PASSWORD: str
-
-    DB_NAME: str
+    # POSTGRES
+    POSTGRES_VERSION: str
+    POSTGRES_DRIVER: str = "postgresql+asyncpg"
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_NAME: str
 
     # DB_WRITE_MIN_POOL_SIZE: int = 4
     # DB_WRITE_MAX_POOL_SIZE: int = 10
-    #
     # DB_READ_MIN_POOL_SIZE: int = 5
     # DB_READ_MAX_POOL_SIZE: int = 20
-    #
     # DB_COMMAND_TIMEOUT: float = 10.0
 
     @computed_field
     @property
-    def DB_URL(self) -> str:
+    def POSTGRES_URL(self) -> str:
         value = (
-            f"{self.DB_DRIVER}://"
-            f"{self.DB_USER}:{self.DB_PASSWORD}@"
-            f"{self.DB_HOST}:{self.DB_PORT}/"
-            f"{self.DB_NAME}"
+            f"{self.POSTGRES_DRIVER}://"
+            f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_NAME}"
         )
         return value
