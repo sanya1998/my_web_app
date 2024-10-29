38967f7d3b6d2555e3e381d4f4389026d94f2c13
Alexander Desyatov <sanya_1998@mail.ru>
2024-10-30 00:22:46 +0300

    2-3-7 fastapi background task

--- app/api/v1/data/images.py
+++ app/api/v1/data/images.py
@@ -1,14 +1,19 @@
 import shutil
 
-from app.common.tasks.img import process_pic
-from fastapi import APIRouter, UploadFile
+from app.common.tasks.img import process_pic, process_pic_background_task
+from fastapi import APIRouter, BackgroundTasks, UploadFile
 
 router = APIRouter(prefix="/images", tags=["Images"])
 
 
 @router.post("/hotels")
-async def add_hotel_image(name: str, file: UploadFile):
+async def add_hotel_image(name: str, file: UploadFile, background_tasks: BackgroundTasks):
     im_path = f"static/images/{name}.webp"
     with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
+
+    # Задачу в celery (плюсы: можно получить результат, можно повторить задачу)
     process_pic.delay(im_path)  # Отправка задачи в очередь celery
+
+    # Задачу во встроенный бэкграунд (плюсы: можно дебажить, можно асинхронные функции выполнять)
+    background_tasks.add_task(process_pic_background_task, im_path)
--- app/common/tasks/img.py
+++ app/common/tasks/img.py
@@ -4,16 +4,21 @@ from app.resources.celery import celery
 from PIL import Image
 
 
+# For celery
 @celery.task
-def process_pic(
-    path: str,
-):
+def process_pic(path: str):
     im_path = Path(path)
     im = Image.open(im_path)
 
     width_big, height_big = 1920, 1080
     im_resized_big = im.resize((width_big, height_big))
     im_resized_big.save(f"static/images/{width_big}_{height_big}/{im_path.name}")
+
+
+# For fastapi background task
+def process_pic_background_task(path: str):
+    im_path = Path(path)
+    im = Image.open(im_path)
 
     width_small, height_small = 100, 100
     im_resized_small = im.resize((width_small, height_small))
