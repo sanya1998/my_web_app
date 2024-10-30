9bbbfdf7d508ef727ef839830c396dfa50d6fb09
Alexander Desyatov <sanya_1998@mail.ru>
2024-10-30 23:20:54 +0300

    refactoring auth Response

--- app/api/v1/users/sign_up.py
+++ app/api/v1/users/sign_up.py
@@ -5,19 +5,15 @@ from app.common.exceptions.api.base import BaseApiError
 from app.common.exceptions.services.already_exists import AlreadyExistsServiceError
 from app.common.exceptions.services.base import BaseServiceError
 from app.common.schemas.user import OneUserReadSchema
-from fastapi import APIRouter, Response
+from fastapi import APIRouter
 
 sign_up_router = APIRouter()
 
 
 @sign_up_router.post("/sign_up")
-async def sign_up(
-    user_input: UserInputDep, auth_service: AuthorizationServiceDep, response: Response
-) -> OneUserReadSchema:
+async def sign_up(user_input: UserInputDep, auth_service: AuthorizationServiceDep) -> OneUserReadSchema:
     try:
-        new_user = await auth_service.sign_up(user_input)
-        auth_service.create_and_remember_access_token(response=response, email=user_input.email)
-        return new_user
+        return await auth_service.sign_up(user_input)
     except AlreadyExistsServiceError:
         raise AlreadyExistsApiError
     except BaseServiceError:
