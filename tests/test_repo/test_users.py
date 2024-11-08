120089902bc64a8a5ff3e16d1a8287a340a344a7
Alexander Desyatov <sanya_1998@mail.ru>
2024-11-08 22:35:43 +0300

    2-5-10 pytest-asyncio, db session

---
+++ tests/test_repo/test_users.py
@@ -0,0 +1,19 @@
+import pytest
+from app.common.exceptions.repositories.not_found import NotFoundRepoError
+from app.repositories.user import UserRepo
+
+
+@pytest.mark.parametrize(
+    "user_id, email, is_exist",
+    [
+        (1, "user@example.com", True),
+        (100500, "...", False),
+    ],
+)
+async def test_get_user_by_id(session, user_id, email, is_exist):
+    try:
+        user = await UserRepo(session).get_object(id=user_id)
+        assert is_exist
+        assert user.email == email
+    except NotFoundRepoError:
+        assert not is_exist
