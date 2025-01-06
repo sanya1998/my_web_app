06768d2b4c09287779a32acd45bde4bba03d8350
Alexander Desyatov <sanya_1998@mail.ru>
2025-01-06 18:46:52 +0300

    3-2-6 add json-logger and custom middleware

---
+++ app/common/constants/log_levels.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class LogLevel(str, Enum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+    def __str__(self) -> str:
+        return str.__str__(self)
--- app/middlewares/middlewares.py
+++ app/middlewares/middlewares.py
@@ -1,7 +1,11 @@
+import time
+
+from app.common.logger import logger
 from app.config.main import settings
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
+from starlette.requests import Request
 
 
 def add_middlewares(app: FastAPI):
@@ -13,3 +17,12 @@ def add_middlewares(app: FastAPI):
         allow_headers=settings.ALLOW_HEADERS,
     )
     app.add_middleware(SessionMiddleware, secret_key=settings.JWT_SECRET_KEY)
+
+    # TODO: сделать единообразно
+    @app.middleware("http")
+    async def add_process_time_header(request: Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        logger.info("Request handling time", extra={"process_time": round(process_time, 4)})
+        return response
